4467177d880e40a232726674c6b6db5960b29b5e
Norman <norman-andre.walorski@hs-bochum.de>
2019-07-02 15:04:06 +0200

    @animos90
    hie

---
+++ Tales Of Mana V1.0/Assets/scrips/SignalListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+// Observer Pattern
+public class SignalListener : MonoBehaviour
+{
+    public SignalEvent signalEvent;
+    public UnityEvent unityEvent;
+
+    public void OnSignalRaised()
+    {
+        unityEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        signalEvent.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        signalEvent.DeRegisterListener(this);
+    }
+}
